a600d4c4e28fafc99b52ca4b136f26b4752ff41d
ruduba <vatbudur@gmail.com>
2025-08-28 19:42:28 +0530

    first working code

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,5 @@
+/// <reference types="p5" />
+
 function make2Darray(){
   let arr = new Array(cols);
   for(let i =0; i<arr.length; i++){
@@ -10,13 +12,31 @@ return arr;
 let grid;
 let cols =10;
 let rows = 10;
+let resolution = 40;
 
 function setup(){
-  
+  createCanvas(400, 400);
+  cols = width/resolution;
+  rows = height/resolution;
   grid = make2Darray(10, 10);
   for(let i=0; i<cols; i++){
     for(let j=0; j<rows; j++){
-    grid[i][j] = random(2);
+      grid[i][j] = floor(random(2));
+    }
+  }
+
+}
+function draw(){
+  background(0);
+  for(let i=0; i<cols; i++){
+    for(let j=0; j<rows; j++){
+      let x = i*resolution;
+      let y = j*resolution;
+      if(grid[i][j]==1){
+        fill(255);
+        rect(x, y, resolution, resolution);
+      }
+      
     }
   }
 

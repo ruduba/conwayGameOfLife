892a433a5fe93788038405e79396cd56cfdd158b
ruduba <vatbudur@gmail.com>
2025-08-28 21:07:52 +0530

    working

--- js/sketch.js
+++ js/sketch.js
@@ -12,32 +12,70 @@ return arr;
 let grid;
 let cols =10;
 let rows = 10;
-let resolution = 40;
+let resolution = 10;
 
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(600, 400);
   cols = width/resolution;
   rows = height/resolution;
-  grid = make2Darray(10, 10);
+
+  grid = make2Darray(cols, rows);
   for(let i=0; i<cols; i++){
     for(let j=0; j<rows; j++){
       grid[i][j] = floor(random(2));
     }
   }
-
 }
+
 function draw(){
   background(0);
+
   for(let i=0; i<cols; i++){
     for(let j=0; j<rows; j++){
       let x = i*resolution;
       let y = j*resolution;
       if(grid[i][j]==1){
         fill(255);
-        rect(x, y, resolution, resolution);
+        stroke(0);
+        rect(x, y, resolution-1, resolution-1);
       }
-      
     }
   }
+  let next = make2Darray(cols, rows);
 
+  for(let i=0; i<cols; i++){
+    for(let j=0; j<rows; j++){
+        let state = grid[i][j];
+        if (i == 0|| i== cols-1|| j ==0|| j==rows-1){
+          next[i][j] = state;
+        } else{
+
+        let sum = 0;
+        let neighbors = countNeighbors(grid, i, j);
+
+        
+
+        if(state == 0 && neighbors == 3){
+          next[i][j] = 1;
+        }
+        else if(state == 1 && (neighbors <2||neighbors>3)){
+          next[i][j] = 0;
+        }else{
+          next[i][j] = state;
+        }
+      }
+      }
+    }
+  grid = next;
+}
+function countNeighbors(grid, x, y){
+  let sum = 0;
+for(let i = -1; i<2; i++){
+  for(let j = -1; j<2; j++){
+    sum+=grid[x+i][y+j];
+  }
+
+}
+  sum -= grid[x][y];
+  return sum;
 }
